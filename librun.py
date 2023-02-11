3eaab1b8326333d724fa19da2aff340ba6f49259
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-02-11 11:29:50 +0300

    Update librun.py

--- librun.py
+++ librun.py
@@ -18,6 +18,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("XoX").ninex()
+	__import__("Jm").menu()
 else:
 	exit(f' Unknow device machine {arc}')
